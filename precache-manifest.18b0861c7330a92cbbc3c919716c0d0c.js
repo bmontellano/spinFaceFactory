38fb5173c1e80c5b026e467dd193e271f033e779
bmontellano <bmontellano@gmail.com>
2018-12-07 16:02:05 -0800

    Updates

---
+++ precache-manifest.18b0861c7330a92cbbc3c919716c0d0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spinFaceFactory/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5c9d6bc027e0290be062",
+    "url": "/spinFaceFactory/static/js/runtime~main.5c9d6bc0.js"
+  },
+  {
+    "revision": "a34c3f68a11ee6ace8c0",
+    "url": "/spinFaceFactory/static/js/main.a34c3f68.chunk.js"
+  },
+  {
+    "revision": "1892c3cb4181e8bf0325",
+    "url": "/spinFaceFactory/static/js/1.1892c3cb.chunk.js"
+  },
+  {
+    "revision": "a34c3f68a11ee6ace8c0",
+    "url": "/spinFaceFactory/static/css/main.c18bbdde.chunk.css"
+  },
+  {
+    "revision": "f60c3df417f4a44080959ab0837dba18",
+    "url": "/spinFaceFactory/index.html"
+  }
+];
